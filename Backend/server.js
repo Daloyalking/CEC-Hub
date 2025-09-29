8f36515add090950c282b2a6816745432915fc43
Daloyalking <yusufridwanayomide.com>
2025-09-29 10:51:36 +0100

    Initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -31,6 +31,7 @@ app.use(
   cors({
     origin: ["http://localhost:5173","https://cechub.netlify.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
 app.use(express.json());
