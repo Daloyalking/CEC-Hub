647442d0f5dee530b196076cd09b6b547f221471
Daloyalking <yusufridwanayomide.com>
2025-10-01 21:22:31 +0100

    force redeploy - clear cache

--- Backend/route/galleryRoute.js
+++ Backend/route/galleryRoute.js
@@ -1,12 +1,6 @@
 import express from "express";
 import multer from "multer";
-import {
-  createGallery,
-  getGalleries,
-  updateGallery,
-  deleteImage,
-  deleteGallery,
-} from "../controller/galleryController.js";
+import { createGallery, getGalleries } from "../controller/galleryController.js";
 
 const galleryRouter = express.Router();
 
@@ -15,10 +9,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 // Routes
-galleryRouter.post("/", upload.array("photos", 10), createGallery);
+galleryRouter.post("/", upload.array("photos", 10), createGallery); // "photos" = field name from frontend
 galleryRouter.get("/", getGalleries);
-galleryRouter.put("/:id", upload.array("photos", 10), updateGallery);
-galleryRouter.delete("/image", deleteImage); // expects {galleryId, public_id}
-galleryRouter.delete("/:id", deleteGallery);
 
 export default galleryRouter;
